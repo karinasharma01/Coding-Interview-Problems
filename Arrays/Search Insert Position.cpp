bbe810ee3d494db2207f56e95d8e9960d5fb4e25
karinasharma01 <68894444+karinasharma01@users.noreply.github.com>
2021-05-19 17:33:01 +0530

    binary search

--- Arrays/Search Insert Position.cpp
+++ Arrays/Search Insert Position.cpp
@@ -35,3 +35,33 @@ public:
         return index;
     }
 };
+
+
+//Method-2
+//O(log n) Time Complexity
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        //O(log n) solution
+        int start=0;
+        int end=nums.size()-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+};
